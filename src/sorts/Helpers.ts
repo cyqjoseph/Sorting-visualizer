15593e2501c52ce63a9b840e22cf9fa527c5c5b9
BunAppleTeeth <cyqjoseph@gmail.com>
2021-12-10 12:03:35 +0800

    removed unnecessary code

--- src/sorts/Helpers.ts
+++ src/sorts/Helpers.ts
@@ -1,5 +1,4 @@
 import { SortBar } from "../models/SortBar";
-import { store } from "../state";
 
 export function getValue(element: SortBar): number {
   return element.height;
@@ -18,18 +17,6 @@ export function compare(x: SortBar, y: SortBar): boolean {
   return getValue(x) >= getValue(y);
 }
 
-export async function sleep(delay: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
-}
-
-export function swap(left: SortBar, right: SortBar): void {
-  let temp: SortBar = left;
-  left = right;
-  right = temp;
-}
-
 export function isSorted(elements: SortBar[]): boolean {
   for (let i = 1; i < elements.length; i++) {
     if (!compare(elements[i], elements[i - 1])) {
@@ -38,10 +25,6 @@ export function isSorted(elements: SortBar[]): boolean {
   }
   return true;
 }
-
-export const invokeRender = function () {
-  return store.getState().sortArray.data;
-};
 
 export const minRunLength = function (n: number): number {
   let MIN_MERGE = 32;
